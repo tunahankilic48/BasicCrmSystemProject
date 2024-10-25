c0ae5c2a9e418840c20fa334e37b1ce8a538fe1e
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-25 23:46:00 +0300

    add user entity and modify role entity(add navigation property)

--- BasicCrmSystem_Domain/Entities/Role.cs
+++ BasicCrmSystem_Domain/Entities/Role.cs
@@ -6,5 +6,8 @@
         public string? Name { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+
+        //Navigation Property
+        public List<User>? Users { get; set; }
     }
 }
