4fb1ffc33a25181cfe98870a4192ee79c8818359
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-25 23:43:56 +0300

    add region entity

---
+++ BasicCrmSystem_Domain/Entities/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCrmSystem_Domain.Entities
+{
+    public class Region : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
