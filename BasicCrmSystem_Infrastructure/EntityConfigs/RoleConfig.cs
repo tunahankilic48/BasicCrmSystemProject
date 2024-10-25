658c075a3a932e2bba8a52ae196c421bb58af34f
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 00:17:29 +0300

    add role config

---
+++ BasicCrmSystem_Infrastructure/EntityConfigs/RoleConfig.cs
@@ -0,0 +1,27 @@
+﻿using BasicCrmSystem_Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace BasicCrmSystem_Infrastructure.EntityConfigs
+{
+    internal class RoleConfig : BaseEntityConfig<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+
+            builder.Property(x => x.Name)
+                .IsRequired(true)
+                .IsUnicode(true)
+                .HasColumnType("NVARCHAR(50)")
+                .HasColumnOrder(2);
+
+            //Foreign Key
+            builder.HasMany(x => x.Users)
+                           .WithOne(x => x.Role)
+                           .HasForeignKey(x => x.RoleId)
+                           .OnDelete(DeleteBehavior.Restrict);
+
+            base.Configure(builder);
+        }
+    }
+}
