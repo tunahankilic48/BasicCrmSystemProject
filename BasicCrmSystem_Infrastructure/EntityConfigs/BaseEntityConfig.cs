1f9dcd3d16c43b25c4cf521e6686cafabad0f6e8
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 00:11:46 +0300

    Base entity config created

---
+++ BasicCrmSystem_Infrastructure/EntityConfigs/BaseEntityConfig.cs
@@ -0,0 +1,16 @@
+﻿using BasicCrmSystem_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BasicCrmSystem_Infrastructure.EntityConfigs
+{
+    internal class BaseEntityConfig<T> : IEntityTypeConfiguration<T> where T : class, IBaseEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.Id).IsRequired(true).HasColumnOrder(1);
+            builder.Property(x => x.CreatedAt).IsRequired(false).HasColumnType("datetime2");
+            builder.Property(x => x.UpdatedAt).IsRequired(false).HasColumnType("datetime2");
+        }
+    }
+}
