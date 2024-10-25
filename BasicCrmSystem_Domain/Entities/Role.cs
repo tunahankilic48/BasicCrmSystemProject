b24b0916429b781072e5042c7b832e4fcc25bfda
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-25 23:44:46 +0300

    add role entity

---
+++ BasicCrmSystem_Domain/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicCrmSystem_Domain.Entities
+{
+    public class Role : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
