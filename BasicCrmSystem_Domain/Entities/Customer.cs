777c8ee9c53cd7d1ac3ac9f51c94c310f4d6b7f6
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-25 23:47:39 +0300

    add customer entity and modify region entity(add navigation property)

---
+++ BasicCrmSystem_Domain/Entities/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace BasicCrmSystem_Domain.Entities
+{
+    public class Customer : IBaseEntity
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public int RegionId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        //Navigation Property
+        public Region? Region { get; set; }
+    }
+}
--- BasicCrmSystem_Domain/Entities/Region.cs
+++ BasicCrmSystem_Domain/Entities/Region.cs
@@ -6,5 +6,8 @@
         public string? Name { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
+
+        //Navigation Property
+        public List<Customer>? Customers { get; set; }
     }
 }
