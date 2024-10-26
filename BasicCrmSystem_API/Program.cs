8a6e5c639d60b689c597a9f0afa9b725758c24cf
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 13:12:23 +0300

    creating an web API project for database connection. Connection string was added in the appsettings.json file. Connection string inject in the program.cs

---
+++ BasicCrmSystem_API/Program.cs
@@ -0,0 +1,31 @@
+using BasicCrmSystem_Infrastructure.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<PostgreSqlDbContext>(x => x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); // Connection string will be taken from appsettings.json file
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
