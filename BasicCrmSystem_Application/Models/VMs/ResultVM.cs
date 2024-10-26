e6e116fe8288625894f1290eee6109c0f638d0ff
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 17:18:19 +0300

    account service created. models and enums that are used in the account service also created

---
+++ BasicCrmSystem_Application/Models/VMs/ResultVM.cs
@@ -0,0 +1,16 @@
+﻿using BasicCrmSystem_Application.Enums;
+
+namespace BasicCrmSystem_Application.Models.VMs
+{
+    public class ResultVM
+    {
+        public ResultVM(Result result, string message)
+        {
+            Result = result;
+            Message = message;
+        }
+
+        public Result Result { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ BasicCrmSystem_Application/Services/AccountService/AccountService.cs
@@ -0,0 +1,31 @@
+﻿using BasicCrmSystem_Application.Enums;
+using BasicCrmSystem_Application.Models.DTOs;
+using BasicCrmSystem_Application.Models.VMs;
+using BasicCrmSystem_Domain.Entities;
+using BasicCrmSystem_Domain.Repositories;
+
+namespace BasicCrmSystem_Application.Services.AccountService
+{
+    internal class AccountService : IAccountService
+    {
+        private readonly IUserRepository _userRepository;
+        public AccountService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<ResultVM> Login(LoginDTO model)
+        {
+            User user = await _userRepository.GetDefault(x => x.Username == model.Username);
+            if (user == null)
+            {
+                return new ResultVM(Result.Failed,"Girilen kullanıcı adı bulunamadı!");
+            }
+            if (user.Password != model.Password)
+            {
+                return new ResultVM(Result.Failed, "Girilen parola yanlış!!!");
+            }
+            return new ResultVM(Result.Success, "Giriş başarılı!");
+        }
+    }
+}
