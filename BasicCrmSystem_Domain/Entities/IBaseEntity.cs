8019d849f8de0a1674035e0483df80751ace5069
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 13:12:58 +0300

    datetime property is made nullable in all entites

--- BasicCrmSystem_Domain/Entities/IBaseEntity.cs
+++ BasicCrmSystem_Domain/Entities/IBaseEntity.cs
@@ -3,7 +3,7 @@
     public interface IBaseEntity
     {
         public int Id { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
 }
--- BasicCrmSystem_Domain/Entities/Region.cs
+++ BasicCrmSystem_Domain/Entities/Region.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
 
         //Navigation Property
