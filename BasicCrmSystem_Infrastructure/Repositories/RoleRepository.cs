92e096c8f9563e82cd49016341fa006a0fb17c10
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 16:55:07 +0300

    Role repository created

---
+++ BasicCrmSystem_Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using BasicCrmSystem_Domain.Entities;
+using BasicCrmSystem_Domain.Repositories;
+using BasicCrmSystem_Infrastructure.DatabaseContext;
+
+namespace BasicCrmSystem_Infrastructure.Repositories
+{
+    public class RoleRepository : BaseRepository<Role>, IRoleRepository
+    {
+        public RoleRepository(PostgreSqlDbContext context) : base(context)
+        {
+        }
+    }
+}
