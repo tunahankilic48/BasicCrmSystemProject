cba933719e4bb628d5a5784a1556a278fe8377c3
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-26 18:47:13 +0300

    Autofac were installed for dependency injection.

---
+++ BasicCrmSystem_Application/IoC/DependencyResolver.cs
@@ -0,0 +1,51 @@
+﻿using Autofac;
+using AutoMapper;
+using BasicCrmSystem_Infrastructure.Repositories;
+using BasicCrmSystem_Domain.Repositories;
+using BasicCrmSystem_Application.Services.AccountService;
+using BasicCrmSystem_Application.Services.CustomerService;
+
+
+namespace BasicCrmSystem_Application.IoC
+{
+    public class DependencyResolver : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Mapper>().As<IMapper>().InstancePerLifetimeScope();
+            builder.RegisterType<UserRepository>().As<IUserRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<RoleRepository>().As<IRoleRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<RegionRepository>().As<IRegionRepository>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerRepository>().As<ICustomerRepository>().InstancePerLifetimeScope();
+
+            builder.RegisterType<AccountService>().As<IAccountService>().InstancePerLifetimeScope();
+            builder.RegisterType<CustomerService>().As<ICustomerService>().InstancePerLifetimeScope();
+
+
+
+
+            #region AutoMapper
+            builder.Register(context => new MapperConfiguration(cfg =>
+            {
+                //Register Mapper Profile
+                cfg.AddProfile<BasicCrmSystem_Application.Mapping.Mapping>(); /// AutoMapper klasörünün altına eklediğimiz Mapping classını bağlıyoruz.
+            }
+            )).AsSelf().SingleInstance();
+
+
+
+            builder.Register(c =>
+            {
+                //This resolves a new context that can be used later.
+                var context = c.Resolve<IComponentContext>();
+                var config = context.Resolve<MapperConfiguration>();
+                return config.CreateMapper(context.Resolve);
+            })
+            .As<IMapper>()
+            .InstancePerLifetimeScope();
+            #endregion
+
+            base.Load(builder);
+        }
+    }
+}
