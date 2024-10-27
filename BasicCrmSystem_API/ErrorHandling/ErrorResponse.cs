ce8def28d9b1d331653a4373263f91c2b94f251d
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-10-28 01:37:50 +0300

    Error Handling implemented

---
+++ BasicCrmSystem_API/ErrorHandling/ErrorResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BasicCrmSystem_API.ErrorHandling
+{
+    public class ErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string Title { get; set; }
+        public string ExceptionMessage { get; set; }
+    }
+}
---
+++ BasicCrmSystem_API/ErrorHandling/ExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace BasicCrmSystem_API.ErrorHandling
+{
+    public class ExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            ErrorResponse response = new ErrorResponse()
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                ExceptionMessage = exception.Message,
+                Title = "Something went wrong"
+            };
+
+
+            await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
+            return (true);
+        }
+    }
+}
